3d45f822fcaa45eb8b87a19d147caba6f666ebe6
Breno Souza <breno.vitoi@gmail.com>
2020-08-26 14:08:45 -0300

    Finalizando o Port Scanner, carregando os arquivos atualizados DIOScanner/dioscanner.py, foi criado os menus 2 e 3 que executam a varredura UDPe a varredura intensa

--- DIOScanner/dioscanner.py
+++ DIOScanner/dioscanner.py
@@ -15,3 +15,33 @@ menu = input(""""\n Escolha o tipo de varredura a ser realizada
                 3 ->  Varredura do Tipo Intensa
                 Digite a opção escolhida: """)
 print("A opção escolhida foi", menu)
+
+if menu == "1":
+    print("Versão do Nmap: ", scanner.nmap_version())
+    scanner.scan(ip, '1-1024', '-v -sS')
+    print(scanner.scaninfo())
+    print("Status do IP: ", scanner[ip].state())
+    print(scanner[ip].all_protocols())
+    print("")
+    print("Portas Abertas: ", scanner[ip]['tcp'].keys())
+
+elif menu == "2":
+    print("Versão do Nmap: ", scanner.nmap_version())
+    scanner.scan(ip, '1-1024', '-v -sU')
+    print(scanner.scaninfo())
+    print("Status do IP: ", scanner[ip].state())
+    print(scanner[ip].all_protocols())
+    print("")
+    print("Portas Abertas: ", scanner[ip]['udp'].keys())
+
+elif menu == "3":
+    print("Versão do Nmap: ", scanner.nmap_version())
+    scanner.scan(ip, '1-1024', '-v -sC')
+    print(scanner.scaninfo())
+    print("Status do IP: ", scanner[ip].state())
+    print(scanner[ip].all_protocols())
+    print("")
+    print("Portas Abertas: ", scanner[ip]['tcp'].keys())
+
+else:
+    print("Escolha uma opção válida!!!")
